228e15bf222663ccc78b5d21cc89f0896896f00a
nguyennp.itedu@gmail.com <nguyennp.itedu@gmail.com@c022d5e0-5074-242b-01d7-e2c9f9914ee4>
2014-09-10 17:21:46 +0000

    ContacBean.java

---
+++ idu-danang/src/main/java/com/idu/judi/beans/ContacBean.java
@@ -0,0 +1,70 @@
+package com.idu.judi.beans;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean
+@RequestScoped
+public class ContacBean {
+	private String fullName;
+	private String email;
+    private String phone;
+    private String content;
+    private String title;
+    
+    public void submit(ActionEvent event) {
+        String msg;
+        if (com.idu.judi.util.Support.sendMailContact(fullName, email, phone, title, content)) {
+            msg = "Feedback send successfully!";
+        } else {
+            msg = "Feedback send successfully!";
+        }
+        this.addMessages(msg);
+    }
+
+    public void addMessages(String msg) {
+        FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, msg, "Message!");
+        FacesContext.getCurrentInstance()
+                .addMessage(null, fm);
+    }
+    public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	  public String getFullName() {
+			return fullName;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public String getPhone() {
+			return phone;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setFullName(String fullName) {
+			this.fullName = fullName;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+}
