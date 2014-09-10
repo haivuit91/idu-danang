38cf11e1fd9842187866eaf7b4f123fbc640f87f
nguyennp.itedu@gmail.com <nguyennp.itedu@gmail.com@c022d5e0-5074-242b-01d7-e2c9f9914ee4>
2014-09-10 17:22:31 +0000

    Constants.java update mail Support.java

---
+++ idu-danang/src/main/java/com/idu/judi/util/Constants.java
@@ -0,0 +1,14 @@
+package com.idu.judi.util;
+
+public class Constants {
+	 public static final String TO_EMAIL = "king.vandalism1@gmail.com";
+	    /**
+	     * type administrator email
+	     */
+	    public static final String FROM_EMAIL = "king.vandalism@gmail.com";
+	    /**
+	     * type password of admin email
+	     */
+	    public static final String PASSWORD_EMAIL = "congtrinh";
+
+}
--- idu-danang/src/main/java/com/idu/judi/util/Support.java
+++ idu-danang/src/main/java/com/idu/judi/util/Support.java
@@ -2,6 +2,16 @@ package com.idu.judi.util;
 
 import java.sql.Date;
 import java.util.Random;
+import java.util.Properties;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Message;
+import javax.mail.MessagingException;
 
 public class Support {
 	public static Date getCurrentDate() {
@@ -21,4 +31,47 @@ public class Support {
 		}
 		return result;
 	}
+	 public static boolean sendMailContact(String fullName, String toEmail, String phone, String title, String content) {
+	        boolean check;
+	        Properties props = new Properties();
+	        props.put("mail.smtp.host", "smtp.gmail.com");
+	        props.put("mail.smtp.socketFactory.port", "465");
+	        props.put("mail.smtp.socketFactory.class",
+	                "javax.net.ssl.SSLSocketFactory");
+	        props.put("mail.smtp.auth", "true");
+	        props.put("mail.smtp.port", "465");
+
+	        Session session = Session.getDefaultInstance(props,
+	                new javax.mail.Authenticator() {
+	                    @Override
+	                    protected PasswordAuthentication getPasswordAuthentication() {
+	                        return new PasswordAuthentication(com.idu.judi.util.Constants.FROM_EMAIL, com.idu.judi.util.Constants.PASSWORD_EMAIL);
+	                    }
+	                });
+
+	        try {
+
+	            Message message = new MimeMessage(session);
+	            message.setFrom(new InternetAddress(com.idu.judi.util.Constants.FROM_EMAIL));
+	            message.setRecipients(Message.RecipientType.TO,
+	                    InternetAddress.parse(Constants.TO_EMAIL));
+	            message.setHeader("Content-Type", "text/plain; charset=UTF-8");
+	            message.setHeader("Content-Encoding", "ISO-8859-9");
+	            message.setSubject("Feedback from users. " + toEmail);
+	            String body = "Title " + title + "\n"
+	                    + "Email: " + toEmail + "\n"
+	                    + "Full Name: " + fullName + "\n"
+	                    + "Phone: " + phone + "\n"
+	                    + "Content: \n" + content;
+	            message.setText(body);
+	            message.setContent(body, "text/plain; charset=UTF-8");
+	            Transport.send(message);
+	            check = true;
+
+	        } catch (MessagingException e) {
+	            check = false;
+	            throw new RuntimeException(e);
+	        }
+	        return check;
+	    }
 }
